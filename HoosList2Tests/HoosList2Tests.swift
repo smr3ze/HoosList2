7855fd97e12d2f5f09d6cabb2ef8c66d664c7c48
Ram Ramkumar <Ram@d-172-26-42-186.bootp.virginia.edu>
2015-10-28 20:40:35 -0400

    Initial Commit

---
+++ HoosList2Tests/HoosList2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HoosList2Tests.swift
+//  HoosList2Tests
+//
+//  Created by Ram Ramkumar on 10/28/15.
+//  Copyright © 2015 Ram Ramkumar. All rights reserved.
+//
+
+import XCTest
+@testable import HoosList2
+
+class HoosList2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
