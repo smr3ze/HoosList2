22792c828e43ad440c3a3bfab42f194bfb4b7a99
Ram Ramkumar <Ram@d-172-26-42-186.bootp.Virginia.EDU>
2015-10-28 23:32:58 -0400

    added camera - not functional

---
+++ CameraViewController.swift
@@ -0,0 +1,71 @@
+//
+//  CameraViewController.swift
+//  HoosList2
+//
+//  Created by Ram Ramkumar on 10/28/15.
+//  Copyright © 2015 Ram Ramkumar. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var takePhoto: UIButton!
+    
+    //var imagePicker: UIImagePickerController!
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+//    @IBAction func loadImageButtonTapped(sender: UIButton) {
+//        imagePicker.allowsEditing = false
+//        imagePicker.sourceType = .PhotoLibrary
+//        
+//        presentViewController(imagePicker, animated: true, completion: nil)
+//    }
+    
+    @IBAction func takePhoto(sender: UIButton) {
+        imagePicker =  UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .Camera
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+//        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+//            ImageView.contentMode = .ScaleAspectFit
+//            ImageView.image = pickedImage
+//        }
+//        
+//        dismissViewControllerAnimated(true, completion: nil)
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        ImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+//    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+//        dismissViewControllerAnimated(true, completion: nil)
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
