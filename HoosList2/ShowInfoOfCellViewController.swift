b0c80a05d8acd9fa80ca250c6142d87c715392c3
Ram Ramkumar <Ram@d-172-26-42-186.bootp.Virginia.EDU>
2015-11-03 19:36:10 -0500

    added corelocation - gps, click 'get location' to see address

--- HoosList2/ShowInfoOfCellViewController.swift
+++ HoosList2/ShowInfoOfCellViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ShowInfoOfCellViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+class ShowInfoOfCellViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, CLLocationManagerDelegate  {
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var cellName: UILabel!
-
     @IBOutlet weak var takePhoto: UIButton!
     
     override func viewDidLoad() {
@@ -24,6 +28,52 @@ class ShowInfoOfCellViewController: UIViewController, UINavigationControllerDele
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func findMyLocation(sender: AnyObject) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error)->Void in
+            
+            if (error != nil)
+            {
+                print("Error: " + error!.localizedDescription)
+                return
+            }
+            
+            if placemarks!.count > 0
+            {
+                let pm = placemarks![0] 
+                self.displayLocationInfo(pm)
+            }
+            else
+            {
+                print("Error with the data.")
+            }
+        })
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Error while updating location " + error.localizedDescription)
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark) {
+        //stop updating location to save battery life
+        self.locationManager.stopUpdatingLocation()
+//        print(placemark.subThoroughfare! + " " + placemark.thoroughfare!)
+//        print(placemark.name!)
+//        print(placemark.locality!)
+//        print(placemark.administrativeArea! + " " + placemark.postalCode!)
+//        print(placemark.country!)
+        locationLabel.numberOfLines = 3
+        locationLabel.text = placemark.subThoroughfare! + " " + placemark.thoroughfare! + ", " + placemark.locality! + ", " + placemark.administrativeArea! + " " + placemark.postalCode!
+    }
+
 
     /*
     // MARK: - Navigation
@@ -39,5 +89,7 @@ class ShowInfoOfCellViewController: UIViewController, UINavigationControllerDele
     override func viewWillAppear(animated:Bool) {
         cellName.text = name;
     }
+    
+    
 
 }
